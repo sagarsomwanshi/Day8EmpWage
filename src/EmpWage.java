5ddc54b567b14e5af1fcaf8da5b2f0e096955b8d
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-16 19:36:40 +0530

    Added : Calculate Daily work hours & Wage

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,12 +1,29 @@
 public class EmpWage {
+    int workHour;
+    int hourWage = 20;
     public static void main(String[] args) {
-        int attendance = UC1();
-        System.out.println(attendance);
-
-
+        UC2();
     }
     public static int UC1(){
-        int attendance = (int) ((Math.random() * 10) % 2);
+        int attendance = (int) ((Math.random() * 10) % 3);
         return attendance;
+    }
+    public static void UC2(){
+        EmpWage E = new EmpWage();
+        switch (UC1()) {
+            case 0 :
+                E.workHour = 8;
+                System.out.println("Employee is present for full day");
+                System.out.println("Daily Wage : " + (E.workHour*E.hourWage));
+                break;
+            case 1:
+                E.workHour = 4;
+                System.out.println("Employee is present for Half day");
+                System.out.println("Daily Wage : " + (E.workHour*E.hourWage));
+                break;
+            default:
+                System.out.println("Employee is Absent");
+        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Day 8 Employee Wage Problem");
+        System.out.println("");
     }
 }
