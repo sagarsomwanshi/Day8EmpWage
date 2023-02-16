c9807c03bfe1574fbfa6c7b1f95a82995a403fb3
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-16 21:14:11 +0530

    Added : Calculates Monthly wage till max working hours & days reached

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,34 +1,45 @@
 public class EmpWage {
-    int workHour;
+    static int workHour,workDay,fullDay,halfDay;
     int hourWage = 20;
-    String emp1= "Regular Employee";
+    String emp1 = "Regular Employee";
     String emp2 = "Part Time Employee";
-
     public static void main(String[] args) {
         EmpWage E = new EmpWage();
-        UC2(E.emp1);
-        UC2(E.emp2);
+        UC5(E.emp1);
+        UC5(E.emp2);
     }
     public static int UC1(){
         int attendance = (int) ((Math.random() * 10) % 3);
         return attendance;
     }
-    public static void UC2(String X ){
+    public static void UC2(){
         EmpWage E = new EmpWage();
         switch (UC1()) {
             case 0 :
-                E.workHour = 8;
-                System.out.println("\n"+X+" is present for full day");
-                System.out.println("Daily Wage : " + (E.workHour*E.hourWage));
+                workHour += 8;
+                workDay++;
+                fullDay++;
                 break;
             case 1:
-                E.workHour = 4;
-                System.out.println("\n"+X+" is present for Half day");
-                System.out.println("Daily Wage : " + (E.workHour*E.hourWage));
+                workHour += 4;
+                workDay++;
+                halfDay++;
                 break;
-            default:
-                System.out.println("\n"+X+" is Absent");
         }
+    }
+    public static void UC5(String X){
+        EmpWage E = new EmpWage();
+        while(workDay <=20 && workHour<100){
+            UC2();
+        }
+        System.out.println("\n"+X+" : ");
+        System.out.println("Full Day : " +fullDay+" Days");
+        System.out.println("Half Day : " +halfDay+" Days");
+        System.out.println("Monthly Wage of " +X+ " : " +(workHour*E.hourWage));
 
+        workDay = 0;
+        workHour = 0;
+        fullDay = 0;
+        halfDay = 0;
     }
 }
